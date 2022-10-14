d10c82b0d4758fe1bdae77c0e1973de9f14aaf5c
Nir Nicole <Niko426@gmail.com>
2022-10-14 11:56:46 +0300

    added cache to the model, and a get dreamteam logic

--- client/src/model.js
+++ client/src/model.js
@@ -4,33 +4,42 @@
 const rupgModel = function () {
 	let nbaMetaInstance
 	let dreamTeamInstance
+	let cacheData
 
-	function initData(year, team, active) {
+	function getCache() {
+		return cacheData
+	}
+
+	function initData(year = 2020, team = "lakers", active = false) {
 		nbaMetaInstance = new MetaDataApi(year, team, active)
 		dreamTeamInstance = new DreamTeamApi()
 	}
 
-	async function getData() {
-		let nbaMetaPromise = nbaMetaInstance.getData()
-		return await Promise.all([nbaMetaPromise]).then(function (results) {
+	async function getData(dreamTeam = false) {
+		let promise = dreamTeam
+			? dreamTeamInstance.getData()
+			: nbaMetaInstance.getData()
+		return await Promise.all([promise]).then(function (results) {
+			cacheData = results[0]
 			return { metaData: results[0] }
 		})
 	}
 
-	async function addPlayer(playerId) {
-		let addedPlayerPromise = dreamTeamInstance.postData(playerId)
+	async function addPlayer(playerData) {
+		let addedPlayerPromise = dreamTeamInstance.postData(playerData)
 		return await Promise.all([addedPlayerPromise]).then(function (results) {
 			return { metaData: results[0] }
 		})
 	}
 
-	async function deletePlayer(playerId) {
-		let addedPlayerPromise = dreamTeamInstance.deleteData(playerId)
+	async function deletePlayer(playerData) {
+		let addedPlayerPromise = dreamTeamInstance.deleteData(playerData)
 		return await Promise.all([addedPlayerPromise]).then(function (results) {
 			return { metaData: results[0] }
 		})
 	}
 	return {
+		getCache,
 		getData,
 		initData,
 		addPlayer,
